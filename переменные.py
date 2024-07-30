f8dbee693648eb11a9a0610474a6a692bd7efdce
kotsaf <safqwer.136@gmail.com>
2024-07-30 19:59:54 +0300

    Add files via upload

--- переменные.py
+++ переменные.py
@@ -1,5 +1,5 @@
 name = 'Python'
 num_hours = 1.5
 num_hw = 12
-av_time = num_hours/num_hw
-print(name, 'Всего задач:', num_hw, 'Затрачено часов:', num_hours, 'Среднее время выполнения:', av_time)
+average_t = num_hours/num_hw
+print(name, 'Всего задач:', num_hw, 'Затрачено часов:', num_hours, 'Среднее время выполнения:', average_t)
