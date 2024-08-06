a44741626cd1ae69d6355141ab3b26eed9cec074
kotsaf <safqwer.136@gmail.com>
2024-08-06 14:49:29 +0300

    Delete переменные.py

--- переменные.py
+++
@@ -1,5 +0,0 @@
-name = 'Python'
-num_hours = 1.5
-num_hw = 12
-average_t = num_hours/num_hw
-print(name, 'Всего задач:', num_hw, 'Затрачено часов:', num_hours, 'Среднее время выполнения:', average_t)
