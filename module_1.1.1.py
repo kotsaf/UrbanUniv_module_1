16ab541d8f19d5c7a440a81514495803988ce170
Safar <safqwer.136@gmail.com>
2024-11-19 20:52:44 +0300

    Add files via upload

---
+++ module_1.1.1.py
@@ -0,0 +1,20 @@
+# 1st program
+print(9 ** 0.5 * 5)
+print()
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+print()
+
+# 3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+print()
+
+# 4th program
+st = '123.456'
+num = float(st) * 10
+res = int(num % 30)
+print(res)
+
